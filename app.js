2452837c55164a433e86784ab351df609a7de37c
Sumit <sumit0311@gmail.com>
2020-10-07 17:52:07 +0530

    App Started

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var path = require('path');
+var app = express();
+
+
+// app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/views'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get("/", function(req,res){
+    res.sendFile("index");
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function(req, res){
+    console.log("Server has started!")
+});
