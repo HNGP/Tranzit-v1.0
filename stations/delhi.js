cb0ae52965f26dbe42bc5f09f05c88482885a4f3
ashwin-sekar <ashwinsekar14@gmail.com>
2020-12-03 14:32:56 +0530

    final deliverable lock

--- stations/delhi.js
+++ stations/delhi.js
@@ -15,10 +15,25 @@ const map = {
       "layout": "Underground",
       "longitude": 77.20771,
       "latitude": 28.56892
+    }
+  },
+  "Akshardham": {
+    "connected": {"Yamuna Bank": 0, "Mayur Vihar -I": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.27869,
+      "latitude": 28.61806
+    }
+  },
+  "Anand Vihar": {
+    "connected": {"Karkarduma": 0, "Kaushambi": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.31603,
+      "latitude": 28.64695
     },
-    "connectedVertices":[
-      3, 5
-    ]
   },
   "Arjan Garh": {
     "connected": {"Guru Dronacharya": 0, "Ghitorni": 0},
@@ -27,6 +42,15 @@ const map = {
       "layout": "Elevated",
       "longitude": 77.12583,
       "latitude": 28.48076
+    }
+  },
+  "Ashok Park Main": {
+    "connected": {"Kirti Nagar": 0, "Inderlok": 0, "Surajmal Stadium": 0},
+    "details": {
+      "line": ["Green Line"],
+      "layout": "Elevated",
+      "longitude": 77.15527,
+      "latitude": 28.67153
     }
   },
   "Azadpur":{
@@ -38,8 +62,26 @@ const map = {
       "latitude": 28.70696
     }
   },
+  "Mandi House": {
+    "connected": {"Rajiv Chowk": 0, "Yamuna Bank": 0, "Kashmere Gate": 0, "Lajpat Nagar": 0},
+    "details": {
+      "line": ["Blue Line", "Violet Line"],
+      "layout": "Underground",
+      "longitude": 77.23410,
+      "latitude": 28.62588
+    }
+  },
+  "Badarpur Border": {
+    "connected": {"Kalkaji Mandir": 0},
+    "details": {
+      "line": ["Violet Line"],
+      "layout": "Elevated",
+      "longitude": 77.30307,
+      "latitude": 28.49334
+    }
+  },
   "Botanical Garden": {
-    "connected": {"Hauz Khas": 0, "Rajiv Chowk": 0, "Noida City Centre": 0},
+    "connected": {"Kalkaji Mandir": 0, "Noida Sector 18": 0, "Noida Golf Course": 0},
     "details": {
       "line": ["Blue Line", "Magenta Line"],
       "layout": "Elevated",
@@ -93,7 +135,7 @@ const map = {
     }
   },
   "Dwarka": {
-    "connected": {"Janakpuri West": 0, "Rajiv Chowk": 0},
+    "connected": {"Janakpuri West": 0, "Kirti Nagar": 0},
     "details": {
       "line": ["Blue Line"],
       "layout": "Elevated",
@@ -148,7 +190,7 @@ const map = {
     } 
   },
   "Hauz Khas": {
-    "connected": {"Saket": 0, "Green Park": 0, "IITD": 0, "Botanical Garden": 0},
+    "connected": {"Saket": 0, "Green Park": 0, "IITD": 0, "Kalkaji Mandir": 0},
     "details": {
     "line": ["Yellow Line","Magenta Line"],
     "layout": "Underground",
@@ -192,6 +234,15 @@ const map = {
       "latitude": 28.57526
     }
   },
+  "Inderlok": {
+    "connected": {"Netaji Subhash Place": 0, "Kashmere Gate": 0, "Ashok Park Main": 0},
+    "details": {
+      "line": ["Red Line", "Green Line"],
+      "layout": "Elevated",
+      "longitude": 77.16994,
+      "latitude": 28.67319
+    }
+  },
   "Jahangirpuri": {
     "connected": {"Azadpur": 0, "Haiderpur": 0},
     "details": {
@@ -204,7 +255,7 @@ const map = {
   "Janakpuri West": {
     "connected" : {"Terminal 1 IGI Airport": 0, "Dwarka": 0},
     "details": {
-      "line": ["Blue Line"],
+      "line": ["Blue Line", "Magenta Line"],
       "layout": "Elevated",
       "longitude": 77.07767,
       "latitude": 28.62943
@@ -219,13 +270,67 @@ const map = {
       "latitude": 28.58708
     }
   },
-  "Kashmere Gate": {
-    "connected": {"Civil Lines": 0, "Chandni Chowk": 0},
+  "Kalkaji Mandir": {
+    "connected": {"Botanical Garden": 0,"Hauz Khas": 0, "Lajpat Nagar": 0, "Badarpur Border": 0},
     "details": {
-      "line": ["Red Line", "Yellow Line"],
-      "layout": "Elevated(RL), Underground(YL)",
+      "line": ["Violet Line", "Magenta Line"],
+      "layout": "Elevated",
+      "longitude": 77.25835,
+      "latitude": 28.55007
+    }
+  },
+  "Karkarduma": {
+    "connected": {"Preet Vihar": 0, "Anand Vihar": 0},
+    "details": {
+      "line": ["Blue Line branch"],
+      "layout": "Elevated",
+      "longitude": 77.30558,
+      "latitude": 28.64849
+    }
+  },
+  "Kashmere Gate": {
+    "connected": {"Civil Lines": 0, "Chandni Chowk": 0, "Mandi House": 0, "Inderlok": 0, "Welcome": 0},
+    "details": {
+      "line": ["Red Line", "Yellow Line", "Violet Line"],
+      "layout": "Elevated(RL), Underground(YL), Underground(VL)",
       "longitude": 77.22817,
       "latitude": 28.66750
+    }
+  },
+  "Kaushambi": {
+    "connected": {"Anand Vihar": 0, "Vaishali": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.32432,
+      "latitude": 28.64544
+    }
+  },
+  "Kirti Nagar": {
+    "connected": {"Dwarka": 0, "Rajiv Chowk": 0, "Ashok Park Main": 0},
+    "details": {
+      "line": ["Blue Line", "Green Line"],
+      "layout": "Elevated",
+      "longitude": 77.15057,
+      "latitude": 28.65575
+    }
+  },
+  "Lajpat Nagar": {
+    "connected": {"Mandi House": 0, "Kalkaji Mandir": 0},
+    "details": {
+      "line": ["Violet Line"],
+      "layout": "Elevated",
+      "longitude": 77.23653,
+      "latitude": 28.57079
+    }
+  },
+  "Laxmi Nagar": {
+    "connected": {"Yamuna Bank": 0, "Nirman Vihar": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.27749,
+      "latitude": 28.63064
     }
   },
   "Lok Kalyan Marg": {
@@ -235,6 +340,24 @@ const map = {
       "layout": "Underground",
       "longitude": 77.21088,
       "latitude": 28.59726
+    }
+  },
+  "Mayur Vihar -I": {
+    "connected": {"Mayur Vihar Extension": 0, "Akshardham": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.28925,
+      "latitude": 28.60442
+    }
+  },
+  "Mayur Vihar Extension": {
+    "connected": {"New Ashok Nagar": 0, "Mayur Vihar -I": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.29455,
+      "latitude": 28.59428
     }
   },
   "MG Road": {
@@ -264,6 +387,24 @@ const map = {
       "latitude": 28.557945
     }
   },
+  "Netaji Subhash Place": {
+    "connected": {"Rithala": 0, "Inderlok": 0},
+    "details": {
+      "line": ["Red Line"],
+      "layout": "Elevated",
+      "longitude": 77.15226,
+      "latitude": 28.69591
+    }
+  },
+  "New Ashok Nagar": {
+    "connected": {"Noida Sector 15": 0, "Mayur Vihar Extension": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.30204,
+      "latitude": 28.58916
+    }
+  },
   "New Delhi": {
     "connected": {"Rajiv Chowk": 0, "Chawri Bazar": 0},
     "details": {
@@ -273,13 +414,58 @@ const map = {
       "latitude": 28.64307
     }
   },
+  "Nirman Vihar": {
+    "connected": {"Laxmi Nagar": 0, "Preet Vihar": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.28683,
+      "latitude": 28.63663
+    }
+  },
   "Noida City Centre": {
-    "connected": {"Botanical Garden": 0},
+    "connected": {"Noida Golf Course": 0},
     "details": {
       "line": ["Blue Line"],
       "layout": "Elevated",
       "longitude": 77.35608,
       "latitude": 28.57466
+    }
+  },
+  "Noida Golf Course": {
+    "connected": {"Noida City Centre": 0, "Botanical Garden": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.34598,
+      "latitude": 28.56714
+    }
+  },
+  "Noida Sector 15": {
+    "connected": {"Noida Sector 16": 0, "New Ashok Nagar": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.31139,
+      "latitude": 28.58512
+    }
+  },
+  "Noida Sector 16": {
+    "connected": {"Noida Sector 18": 0, "Noida Sector 15": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.31757,
+      "latitude": 28.57819
+    }
+  },
+  "Noida Sector 18": {
+    "connected": {"Botanical Garden": 0, "Noida Sector 16": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.32612,
+      "latitude": 28.57081
     }
   },
   "Patel Chowk": {
@@ -291,13 +477,31 @@ const map = {
       "latitude": 28.62295
     }
   },
+  "Preet Vihar": {
+    "connected": {"Nirman Vihar": 0, "Karkarduma": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.29543,
+      "latitude": 28.64171
+    }
+  },
   "Rajiv Chowk": {
-    "connected": {"Central Secretariat": 0,"Botanical Garden": 0, "Dwarka": 0, "New Delhi": 0},
+    "connected": {"Central Secretariat": 0, "Mandi House": 0, "Kirti Nagar": 0, "New Delhi": 0},
     "details": {
       "line": ["Yellow Line", "Blue Line"],
       "layout": "Underground",
       "longitude": 77.21826,
       "latitude": 28.63282
+    }
+  },
+  "Rithala": {
+    "connected": {"Netaji Subhash Place": 0},
+    "details": {
+      "line": ["Red Line"],
+      "layout": "Elevated",
+      "longitude": 77.10713,
+      "latitude": 28.72072
     }
   },
   "Rohini Sector 18-19": {
@@ -327,6 +531,15 @@ const map = {
       "latitude": 28.746521
     }
   },
+  "Shahdara": {
+    "connected": {"Welcome": 0},
+    "details": {
+      "line": ["Red Line"],
+      "layout": "At Grade",
+      "longitude": 77.28962,
+      "latitude": 28.67345
+    }
+  },
   "Sikandarpur": {
     "connected": {"Guru Dronacharya": 0, "MG Road": 0},
     "details": {
@@ -345,6 +558,15 @@ const map = {
       "latitude": 28.49927
     }
   },
+  "Surajmal Stadium": {
+    "connected": {"Ashok Park Main": 0},
+    "details": {
+      "line": ["Green Line"],
+      "layout": "Elevated",
+      "longitude": 77.07385,
+      "latitude": 28.68180
+    }
+  },
   "Terminal 1 IGI Airport": {
     "connected": {"Munirka": 0, "Janakpuri West": 0},
     "details": {
@@ -361,6 +583,15 @@ const map = {
       "layout": "Elevated",
       "longitude": 77.18648,
       "latitude": 28.51302
+    }
+  },
+  "Vaishali": {
+    "connected": {"Kaushambi": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "Elevated",
+      "longitude": 77.33974,
+      "latitude": 28.64997
     }
   },
   "Vidhan Sabha": {
@@ -390,6 +621,24 @@ const map = {
       "latitude": 28.61166
     }
   },
+  "Welcome": {
+    "connected": {"Kashmere Gate": 0, "Shahdara": 0},
+    "details": {
+      "line": ["Red Line"],
+      "layout": "At Grade",
+      "longitude": 77.27756,
+      "latitude": 28.67180
+    }
+  },
+  "Yamuna Bank": {
+    "connected": {"Akshardham": 0, "Laxmi Nagar": 0, "Mandi House": 0},
+    "details": {
+      "line": ["Blue Line"],
+      "layout": "At-Grade",
+      "longitude": 77.26792,
+      "latitude": 28.62331
+    }
+  }
 }
 
 module.exports = {
